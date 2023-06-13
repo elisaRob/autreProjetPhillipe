ef9321c3fc4681b614e349da899f5f7cfb61dd1d
Elisabeth ROBERT AOUCHER <elisabeth.robert.aoucher@gmail.com>
2023-06-13 20:52:44 +0200

    continuation

--- afficherVillesEtCodePostaux.php
+++ afficherVillesEtCodePostaux.php
@@ -1,15 +1,42 @@
 <?php
 
 require_once('bdd.php');
+require_once('header.php');
 
 $bdd=connexionBDD();
 
-$recupererVilleEtCodePostaux="SELECT ville_nom,ville_code_postal FROM villes_france_free ";
+$recupererVilleEtCodePostaux="SELECT ville_nom,ville_code_postal FROM villes_france_free LIMIT 10";
 $requete=$bdd->prepare($recupererVilleEtCodePostaux);
 $requete->execute();
 
-// $villeEtCodePostauxRecupere=$requete->fetch(PDO::FETCH_ASSOC);
+?>
+<div class="recupererVilleEtCodePostaux">
 
+<table>
+        <thead>
+            <th>Villes</th>
+            <th>Code postal</th>
+        </thead>
+        <tbody>
+<?php
 while ($villeEtCodePostauxRecupere = $requete->fetch(PDO::FETCH_ASSOC)) {
-    echo $villeEtCodePostauxRecupere['ville_nom'] . ' - ' . $villeEtCodePostauxRecupere['ville_code_postal'] . '<br>';
-}
+    ?>
+   
+    
+                <tr>
+                    <td><?=$villeEtCodePostauxRecupere['ville_nom']?></td>
+                    <td><?=$villeEtCodePostauxRecupere['ville_code_postal']?></td>
+                </tr>
+<?php
+            }
+?>
+              
+            </tbody>
+        </table>
+
+    </div>
+
+    
+    <?php
+    require_once('footer.php');
+   
--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="app.js"></script>
-</head>
-<body>
+<?php
+
+require_once('header.php');
+?>
+
     <button><a href="afficherVillesEtCodePostaux.php">Afficher Villes Et Code postaux</a></button>
     <button><a href="">Formulaire Inscription Ville</a></button>
     <button><a href="">Modifier code postal ville</a></button>
     <button><a href="">Supprimer Ville</a></button>
-</body>
-</html>
+
+    <?php
+
+require_once('footer.php');
+?>
