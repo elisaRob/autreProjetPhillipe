ec37284947c19a9608e7d59e57a63f64811dbb11
Elisabeth ROBERT AOUCHER <elisabeth.robert.aoucher@gmail.com>
2023-06-16 20:43:43 +0200

    rajout css

--- formulaireInscriptionVille.php
+++ formulaireInscriptionVille.php
@@ -6,6 +6,7 @@ require_once('bdd.php');
 ?>
 
 <div class="containerFormulaireInscriptionNouvelleVille">
+    <h2>Nouvelle ville à ajouter à la base de donnée</h2>
     <form action="insertionNouvelleVille.php" method="POST">
         <label for="ville">Entrez le nom de votre ville</label>
         <input type="text" id="ville" name="ville">
